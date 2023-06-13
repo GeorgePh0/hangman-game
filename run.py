9de1a1e3abefbf7bcf1c0a22403a337b6a731229
GeorgePh0 <george.philip0020@gmail.com>
2023-06-13 14:02:12 +0000

    Add function to clear console and to grab a random word from word file

--- run.py
+++ run.py
@@ -4,3 +4,19 @@ from words import word_list
 import random
 # clears terminal
 import os
+
+
+def clear_console():
+    """
+    Function to clear terminal
+    """
+    os.system('clear')
+
+
+# Selects the word at random
+def grab_word():
+    """
+    Function to pick a random word from the list in the saved data
+    """
+    word = random.choice(word_list)
+    return word.upper()
