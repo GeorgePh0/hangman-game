8ff3c8833b9f0d85231b03f9ee6793ff71a7648a
GeorgePh0 <george.philip0020@gmail.com>
2023-06-13 14:03:43 +0000

    Add function for welcome message and ask for player name

--- run.py
+++ run.py
@@ -20,3 +20,37 @@ def grab_word():
     """
     word = random.choice(word_list)
     return word.upper()
+
+
+# Gives the player a welcome message and asks for their name
+def welcome():
+    """
+    Function that prints the opening message,
+    and also asks player to input their name
+    """
+    print("Welcome to hangman. \n\n"
+          "You have 6 guesses to complete the word. \n\n"
+          "All words are from football teams in England. \n\n")
+    player = " "
+    while True:
+        player = input("Please enter your name: ")
+        if player.isalpha() is not True:
+            print("Error: Please enter letters only.")
+            continue
+        else:
+            print("\n")
+            print(f'Hi {player}, welcome and also GOOD LUCK.')
+            return player
+
+
+# Loads the terminal and starts the gmae
+def main():
+    """
+    Main Function to play the game
+    """
+    word = grab_word()
+    welcome()
+
+
+if __name__ == "__main__":
+    main()
